103f8b61451d0cd0d93a5403e03cb9a569103845
Han Chang <szu.han.chang@gmail.com>
2013-10-17 22:00:19 +0000

    Do not show on home/front page

--- amazon-affiliate-affix.php
+++ amazon-affiliate-affix.php
@@ -23,35 +23,35 @@ function amazon_affiliate_affix_scripts() {
 
 class AmazonAffiliateAffix extends WP_Widget {
 
-	public function __construct() {
-		parent::__construct(
-			'AmazonAffiliateAffix', // Base ID
-			__('Amazon Affiliate Affix', 'text_domain'), // Name
-			array( 'description' => __( 'Amazon Affiliate Affix Description', 'text_domain' ), ) // Args
-		);
-	}
+  public function __construct() {
+    parent::__construct(
+      'AmazonAffiliateAffix', // Base ID
+      __('Amazon Affiliate Affix', 'text_domain'), // Name
+      array( 'description' => __( 'Amazon Affiliate Affix Description', 'text_domain' ), ) // Args
+    );
+  }
 
-	/**
-	 * Front-end display of widget.
-	 *
-	 * @see WP_Widget::widget()
-	 *
-	 * @param array $args     Widget arguments.
-	 * @param array $instance Saved values from database.
-	 */
-	public function widget( $args, $instance ) {
+  /**
+   * Front-end display of widget.
+   *
+   * @see WP_Widget::widget()
+   *
+   * @param array $args     Widget arguments.
+   * @param array $instance Saved values from database.
+   */
+  public function widget( $args, $instance ) {
     $products = $this->aaa_products(get_the_ID());
 
-    if (empty($products)) {
+    if (empty($products) || is_front_page() || is_home()) {
       return;
     }
 
-		$title = apply_filters( 'widget_title', $instance['title'] );
+    $title = apply_filters( 'widget_title', $instance['title'] );
 
-		echo $args['before_widget'];
+    echo $args['before_widget'];
 
-		if ( ! empty( $title ) ) {
-			echo $args['before_title'] . $title . $args['after_title'];
+    if ( ! empty( $title ) ) {
+      echo $args['before_title'] . $title . $args['after_title'];
     }
 
     foreach ($products as $product) {
@@ -69,49 +69,49 @@ class AmazonAffiliateAffix extends WP_Widget {
 <?php
     }
 
-		echo $args['after_widget'];
-	}
+    echo $args['after_widget'];
+  }
 
-	/**
-	 * Back-end widget form.
-	 *
-	 * @see WP_Widget::form()
-	 *
-	 * @param array $instance Previously saved values from database.
-	 */
- 	public function form( $instance ) {
-		if ( isset( $instance[ 'title' ] ) ) {
-			$title = $instance[ 'title' ];
-		}
-		else {
-			$title = __( 'New title', 'text_domain' );
-		}
+  /**
+   * Back-end widget form.
+   *
+   * @see WP_Widget::form()
+   *
+   * @param array $instance Previously saved values from database.
+   */
+  public function form( $instance ) {
+    if ( isset( $instance[ 'title' ] ) ) {
+      $title = $instance[ 'title' ];
+    }
+    else {
+      $title = __( 'New title', 'text_domain' );
+    }
 ?>
-		<p>
-		<label for="<?php echo $this->get_field_id( 'title' ); ?>">
-			<?php _e( 'Title:' ); ?>
-		</label> 
-		<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
-		</p>
+    <p>
+    <label for="<?php echo $this->get_field_id( 'title' ); ?>">
+      <?php _e( 'Title:' ); ?>
+    </label> 
+    <input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+    </p>
 <?php 
-	}
+  }
 
-	/**
-	 * Sanitize widget form values as they are saved.
-	 *
-	 * @see WP_Widget::update()
-	 *
-	 * @param array $new_instance Values just sent to be saved.
-	 * @param array $old_instance Previously saved values from database.
-	 *
-	 * @return array Updated safe values to be saved.
-	 */
-	public function update( $new_instance, $old_instance ) {
-		$instance = array();
-		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+  /**
+   * Sanitize widget form values as they are saved.
+   *
+   * @see WP_Widget::update()
+   *
+   * @param array $new_instance Values just sent to be saved.
+   * @param array $old_instance Previously saved values from database.
+   *
+   * @return array Updated safe values to be saved.
+   */
+  public function update( $new_instance, $old_instance ) {
+    $instance = array();
+    $instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
 
-		return $instance;
-	}
+    return $instance;
+  }
 
   public function aaa_products( $post_id, $action = 'get', $product_url = '' ) {//, $product_name = '', $product_description = '' ) {
     switch ($action) {
@@ -123,7 +123,7 @@ class AmazonAffiliateAffix extends WP_Widget {
       else {
         return false; 
       }
-      
+
       break;
     case 'get' :
       return $this->parse_amazon_product_meta(get_post_meta($post_id, 'amazon_product'));
